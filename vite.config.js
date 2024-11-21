8fb51e96785126b2308462c930fe0fc3ba222660
Ritesh Pandit <134190903+R17358@users.noreply.github.com>
2024-11-21 15:08:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  base: '/my-portfolio/', // Replace with your repo name
-};
-
+export default defineConfig({
+  plugins: [react()],
+})
